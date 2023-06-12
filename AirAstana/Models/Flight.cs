26b7e1800cb83bea1136f70c1905c6b1d5956fa5
Oleg Kogay <olegatorkogay@gmail.com>
2023-06-12 15:51:20 +0600

    Create Model For Flight

---
+++ AirAstana/Models/Flight.cs
@@ -0,0 +1,18 @@
+﻿namespace AirAstana.Models;
+
+public class Flight
+{
+    public int Id { get; set; }
+    public string Origin { get; set; }
+    public string Destination { get; set; }
+    public DateTimeOffset Departure { get; set; }
+    public DateTimeOffset Arrival { get; set; }
+    public enum MyEnum
+    {
+        InTime,
+        Delayed,
+        Cancelled
+    }
+
+    
+}
