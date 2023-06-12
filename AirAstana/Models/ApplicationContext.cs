4047cd0fcf84632e9451b3d8a5e224d0a4411fd6
Oleg Kogay <olegatorkogay@gmail.com>
2023-06-12 16:37:54 +0600

    Add ApplicationContext

---
+++ AirAstana/Models/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AirAstana.Models;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Flight> Flights { get; set; } = null!;
+    public DbSet<Role> Roles { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+    }
+}
--- AirAstana/Program.cs
+++ AirAstana/Program.cs
@@ -1,8 +1,16 @@
+using AirAstana.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
