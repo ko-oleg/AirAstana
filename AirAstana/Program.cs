407d6279d39708dae4015394ce09e8e4128a570c
Oleg Kogay <olegatorkogay@gmail.com>
2023-06-12 16:29:47 +0600

    Connection EF

--- AirAstana/Program.cs
+++ AirAstana/Program.cs
@@ -9,10 +9,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-    app.UseSwagger();
+    app.UseSwagger(options =>
+    {
+        options.SerializeAsV2 = true;
+    });
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
